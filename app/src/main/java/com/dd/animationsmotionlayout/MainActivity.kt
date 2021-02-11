84b4c67d7de5e9e55ed14accdf5b1b7adf587c16
Daniyar <dds861@gmail.com>
2021-02-11 10:06:32 +0100

    Add view binding

--- app/src/main/java/com/dd/animationsmotionlayout/MainActivity.kt
+++ app/src/main/java/com/dd/animationsmotionlayout/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.dd.animationsmotionlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dd.animationsmotionlayout.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
